46274a778c237c50f393556e5448741e07d594d4
Mirko <mirko.kaempf@gmail.com>
2018-03-27 19:07:25 +0200

    PREP TUTORIAL

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+scp /GITHUB/hadoop-ts-ng/target/hadoop-ts-ng-2.5.0-jar-with-dependencies.jar root@cc-poc-mk-1.gce.cloudera.com:/root/blog-scientific-image-processing-at-scale
+
+scp /GITHUB/hadoop-ts-ng/target/hadoop-ts-ng-2.5.0.jar root@cc-poc-mk-1.gce.cloudera.com:/root/blog-scientific-image-processing-at-scale
+
