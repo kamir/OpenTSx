71ecb2f14ebd08d13f5b20b98e7e9873f7efef82
Mirko Kämpf <mirko.kaempf@gmail.com>
2019-06-26 15:15:17 +0200

    Finalize Demonstrator WF

--- bin/050_installArtifacts_in_local_mvn_repo.sh
+++ bin/050_installArtifacts_in_local_mvn_repo.sh
@@ -7,10 +7,18 @@
 export VERSION=2.5.0
 
 #-------------------------------------------------------------------------------
-# Deploy to local MVN repo
+# Deploy external dependencies to local MVN repository
 #-------------------------------------------------------------------------------
+#
 
+#
+# https://github.com/jlizier/jidt/releases/tag/v1.3.1
+#
 mvn install:install-file -Dfile=./../scripts/infodynamics.jar -DgroupId=infodynamics -DartifactId=corelib -Dversion=1.3.1 -Dpackaging=jar -DskipTests
+
+#-------------------------------------------------------------------------------
+# Deploy binary release to local MVN repository
+#-------------------------------------------------------------------------------
 
 mvn install:install-file -Dfile=./../target/hadoop-ts-ng-$VERSION.jar -DgroupId=com.cloudera -DartifactId=hadoop-ts-ng -Dversion=$VERSION -Dpackaging=jar -DskipTests
 
