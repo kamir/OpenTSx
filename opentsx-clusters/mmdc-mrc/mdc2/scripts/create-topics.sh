4764922e8dd4cba6ef4ef002f227b741235dc352
Mirko Kämpf <mirko@confluent.io>
2020-04-27 12:34:54 +0000

    added setup for MDC2

---
+++ opentsx-clusters/mmdc-mrc/mdc2/scripts/create-topics.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo -e "\n==> Creating topic single-region"
+
+docker-compose exec broker-west-1 kafka-topics  --create \
+	--bootstrap-server broker-west-1:19091 \
+	--topic single-region \
+	--partitions 1 \
+	--replica-placement /etc/kafka/demo/placement-single-region.json \
+	--config min.insync.replicas=1
+
+echo -e "\n==> Creating topic multi-region-sync"
+
+docker-compose exec broker-west-1 kafka-topics  --create \
+	--bootstrap-server broker-west-1:19091 \
+	--topic multi-region-sync \
+	--partitions 1 \
+	--replica-placement /etc/kafka/demo/placement-multi-region-sync.json \
+	--config min.insync.replicas=1
+
+echo -e "\n==> Creating topic multi-region-async"
+
+docker-compose exec broker-west-1 kafka-topics  --create \
+	--bootstrap-server broker-west-1:19091 \
+	--topic multi-region-async \
+	--partitions 1 \
+	--replica-placement /etc/kafka/demo/placement-multi-region-async.json \
+	--config min.insync.replicas=1
+
+echo -e "\n==> Creating topic multi-region-default"
+
+# Note that the --replica-placement flag is not needed because we want to use
+# the default placement constraints in this topic
+docker-compose exec broker-west-1 kafka-topics  \
+	--create \
+	--bootstrap-server broker-west-1:19091 \
+	--topic multi-region-default \
+	--config min.insync.replicas=1
