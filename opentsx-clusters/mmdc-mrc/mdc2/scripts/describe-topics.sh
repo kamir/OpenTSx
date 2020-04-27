c966c598c65a6a6c8a4792cd23635a3cb4c2954d
Mirko Kämpf <mirko@confluent.io>
2020-04-27 12:34:54 +0000

    added setup for MDC2

---
+++ opentsx-clusters/mmdc-mrc/mdc2/scripts/describe-topics.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "\n==> Describe topic single-region\n"
+
+docker-compose exec broker-east-3 kafka-topics --describe \
+	--bootstrap-server broker-east-3:19093 --topic single-region
+
+echo -e "\n==> Describe topic multi-region-sync\n"
+
+docker-compose exec broker-east-3 kafka-topics --describe \
+	--bootstrap-server broker-east-3:19093 --topic multi-region-sync
+
+echo -e "\n==> Describe topic multi-region-async\n"
+
+docker-compose exec broker-east-3 kafka-topics --describe \
+        --bootstrap-server broker-east-3:19093 --topic multi-region-async
+
+echo -e "\n==> Describe topic multi-region-default\n"
+
+docker-compose exec broker-east-3 kafka-topics --describe \
+        --bootstrap-server broker-east-3:19093 --topic multi-region-default
