3c38b00ddee5e333201a5b490b03fd079baef4d1
Mirko Kämpf <mirko@confluent.io>
2020-04-30 15:43:33 +0200

    Prepare the stretched cluster setup at home.

--- opentsx-clusters/mmdc-mrc/mdc2/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc2/scripts/scenario1/run.sh
@@ -1,11 +1,12 @@
 #sudo docker-compose exec cli-west-2 kafka-topics --list --bootstrap-server broker-west-1:9091
 
-sudo docker-compose exec cli-west-2 kafka-topics --create --topic test-szenario-01 --replication-factor 3 --partitions 108 --bootstrap-server broker-west-1:9091
+#sudo docker-compose exec cli-west-2 kafka-topics --create --topic test-szenario-01 --replication-factor 3 --partitions 108 --bootstrap-server broker-west-1:9091
 
-echo "bootstrap-server=broker-west-1:9091" >> config
+echo "bootstrap-server=broker-west-1:9091" >> prod.config
 
-sudo docker cp config cli-west-2:config
+sudo docker cp prod.config cli-west-2:config
 
-sudo docker-compose exec cli-west-2 kafka-producer-perf-test --topic test-szenario-01 --record-size 512 --throughput 60000 --num-records 54000000 --producer-props acks=all linger.ms=10 --producer.config /config | tee /tmp/producer &
+sudo docker-compose exec cli-west-2 kafka-producer-perf-test --topic test-szenario-01 --record-size 512 --throughput 60000 --num-records 54000000 --producer-props acks=all linger.ms=10 --producer.config /config
+#| tee /tmp/producer &
 
 #sudo docker-compose exec cli-west-2 kafka-topics --list --bootstrap-server broker-west-1:9091
