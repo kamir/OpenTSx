e4aaa27cd89ca1b0eceac7ee4c9eb27802d52187
Mirko Kämpf <mirko@confluent.io>
2020-04-30 18:55:20 +0200

    Prepare the stretched cluster setup at home.

--- opentsx-clusters/mmdc-mrc/mdc3/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc3/scripts/scenario1/run.sh
@@ -1,3 +1,33 @@
-sudo docker-compose exec cli-east-1 kafka-topics --list --bootstrap-server 192.168.0.9:9091
+export TN=T2.8
+export ZM=5000000
 
-sudo docker-compose exec cli-east-1 kafka-topics --create --topic test-szenario-01 --replication-factor 3 --partitions 108 --bootstrap-server 192.168.0.9:9091
+echo "*******************"
+echo "Topicname: $TN"
+echo "*******************"
+
+rm -rf tmp_consumer1
+rm -rf tmp_consumer2
+rm -rf tmp_consumer3
+
+sudo docker-compose exec cli-east-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-1:9091 > tmp_consumer1
+sudo docker-compose exec cli-east-2 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-2 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-2:9092 > tmp_consumer2
+sudo docker-compose exec cli-east-3 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-3 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-1:9091 > tmp_consumer3
+
+cat tmp_consumer1
+cat tmp_consumer2
+cat tmp_consumer3
+
+echo "Consumer 1:" \
+    `cat tmp_consumer1 \
+     | awk -F"," '{if($8>0){msec+=$8};mb=$3}END{print mb*1000/msec}'` \
+    "MB/sec"
+
+echo "Consumer 2:" \
+    `cat tmp_consumer2 \
+     | awk -F"," '{if($8>0){msec+=$8};mb=$3}END{print mb*1000/msec}'` \
+    "MB/sec"
+
+echo "Consumer 3:" \
+    `cat tmp_consumer3 \
+     | awk -F"," '{if($8>0){msec+=$8};mb=$3}END{print mb*1000/msec}'` \
+    "MB/sec
