024822fcfbb3c6f35383627a021e9cd098ed4021
Mirko Kämpf <mirko@confluent.io>
2020-04-30 17:03:03 +0200

    Prepare the stretched cluster setup at home.

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
@@ -1,11 +1,12 @@
 export TN=T2.4
-export ZM=100000
+export ZM=1000000
 
 echo "*******************"
 echo "Topicname: $TN"
 echo "*******************"
 
 rm -rf tmp_consumer1
+
 sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-1:9091 > tmp_consumer1
 
 cat tmp_consumer1
--- opentsx-clusters/mmdc-mrc/mdc2/scripts/scenario1/clean.sh
+++ opentsx-clusters/mmdc-mrc/mdc2/scripts/scenario1/clean.sh
@@ -1,4 +1,6 @@
 export TN=T2.4
+export ZM=1000000
+
 
 sudo docker-compose exec cli-west-2 kafka-topics --delete --topic $TN --bootstrap-server 192.168.0.9:9091
 
