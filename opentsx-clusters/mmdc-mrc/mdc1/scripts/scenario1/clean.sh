3612c7e42bd36b64a84ed19b58ae6cbb4c1c78d2
Mirko Kämpf <mirko@confluent.io>
2020-04-30 15:15:30 +0200

    Prepare the stretched cluster setup at home.

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/clean.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/clean.sh
@@ -1,4 +1,4 @@
-sudo docker-compose exec cli-west-1 kafka-topics --delete test-szenario-01 --bootstrap-server 192.168.0.9:9091
+sudo docker-compose exec cli-west-1 kafka-topics --delete --topic test-szenario-01 --bootstrap-server 192.168.0.9:9091
 
 sudo docker-compose exec cli-west-1 kafka-topics --list --bootstrap-server 192.168.0.9:9091
 
