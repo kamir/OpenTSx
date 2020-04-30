2d2da00d47b738659806fdbd83aeeb590946dc4f
Mirko Kämpf <mirko@confluent.io>
2020-04-30 15:24:08 +0200

    Prepare the stretched cluster setup at home.

--- opentsx-clusters/mmdc-mrc/mdc2/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc2/scripts/scenario1/run.sh
@@ -2,4 +2,6 @@ sudo docker-compose exec cli-west-2 kafka-topics --list --bootstrap-server broke
 
 sudo docker-compose exec cli-west-2 kafka-topics --create --topic test-szenario-01 --replication-factor 3 --partitions 108 --bootstrap-server broker-west-1:9091
 
+sudo docker-compose exec cli-west-2 kafka-producer-perf-test --topic test-szenario-01 --record-size 512 --throughput 60000 --num-records 54000000 --producer-props acks=all linger.ms=10 --bootstrap-server broker-west-1:9091 | tee /tmp/producer &
+
 sudo docker-compose exec cli-west-2 kafka-topics --list --bootstrap-server broker-west-1:9091
