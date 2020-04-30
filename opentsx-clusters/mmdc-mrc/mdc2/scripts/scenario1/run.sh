93cf4d919dc4e52b79c29cb784e21acff412e9f0
Mirko Kämpf <mirko@confluent.io>
2020-04-30 16:48:31 +0200

    Prepare the stretched cluster setup at home.

--- opentsx-clusters/mmdc-mrc/mdc2/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc2/scripts/scenario1/run.sh
@@ -1,16 +1,28 @@
 export TN=T2.2
+export ZM=1000
 
 echo "*******************"
 echo "Topicname: $TN"
 echo "*******************"
+echo ""
 
 sudo docker-compose exec cli-west-2 kafka-topics --create --topic $TN --replication-factor 3 --partitions 108 --bootstrap-server broker-west-1:9091
 
 rm -rf prod.config
 echo "bootstrap.servers=broker-west-1:9091" >> prod.config
 
-sudo docker cp prod.config cli-west-2:config
+echo ""
+echo "*******************"
+cat prod.config
+echo "*******************"
+echo ""
 
-sudo docker-compose exec cli-west-2 kafka-producer-perf-test --topic $TN --record-size 512 --throughput 60000 --num-records 54000000 --producer-props acks=all linger.ms=10 --producer.config /config > tmp_producer
+sudo docker cp prod.config cli-west-2:config
+echo "- copied CFG to image ..."
+echo "- started producer for $ZM messages ..."
+
+sudo docker-compose exec cli-west-2 kafka-producer-perf-test --topic $TN --record-size 512 --throughput 60000 --num-records $ZM --producer-props acks=all linger.ms=10 --producer.config /config > tmp_producer
+
+cat tmp_producer
 
 sudo docker-compose exec cli-west-2 kafka-topics --describe --topic $TN
