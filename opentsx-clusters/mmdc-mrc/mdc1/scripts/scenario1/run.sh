6089976739da01db7d623306f5df2959606fe0bb
Mirko Kämpf <mirko@confluent.io>
2020-04-30 17:20:11 +0200

    Prepare the stretched cluster setup at home.

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
@@ -1,5 +1,5 @@
-export TN=T2.5
-export ZM=1000000
+export TN=T2.6
+export ZM=3000000
 
 echo "*******************"
 echo "Topicname: $TN"
@@ -8,8 +8,8 @@ echo "*******************"
 rm -rf tmp_consumer1
 
 sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-1:9091 > tmp_consumer1 &
-sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-2 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-2:9092 > tmp_consumer2 &
-sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-3 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-1:9091 > tmp_consumer3 &
+sudo docker-compose exec cli-west-2 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-2 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-2:9092 > tmp_consumer2 &
+sudo docker-compose exec cli-west-3 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-3 --show-detailed-stats --hide-header --timeout 60000 --bootstrap-server broker-west-1:9091 > tmp_consumer3 &
 
 cat tmp_consumer1
 cat tmp_consumer2
