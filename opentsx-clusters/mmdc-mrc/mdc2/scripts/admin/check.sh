5ad4eb3b195ce8c86a0c92ebfdbfd005b671b794
Mirko Kämpf <mirko@confluent.io>
2020-05-20 14:19:13 +0200

    added stop script

--- opentsx-clusters/mmdc-mrc/mdc2/scripts/admin/check.sh
+++ opentsx-clusters/mmdc-mrc/mdc2/scripts/admin/check.sh
@@ -1,1 +1,1 @@
-sudo docker-compose -f /home/ubuntu/OpenTSx/opentsx-clusters/mmdc-mrc/mdc2/docker-compose.yml disown
+sudo docker-compose -f /home/ubuntu/OpenTSx/opentsx-clusters/mmdc-mrc/mdc2/docker-compose.yml ps
