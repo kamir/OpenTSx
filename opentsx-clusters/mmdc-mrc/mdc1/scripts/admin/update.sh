608ee45e08c1ec91bcac8c7fc1dfcceed10ba74d
Mirko Kämpf <mirko@confluent.io>
2020-05-26 19:45:01 +0200

    Added the load-generator DEMO for DOCKER-COMPOSE

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/admin/update.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/admin/update.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/OpenTSx
-git pull
+sudo git pull
 cd opentsx-clusters/mmdc-mrc/mdc1
 source .env
 sudo docker-compose -f /home/ubuntu/OpenTSx/opentsx-clusters/mmdc-mrc/mdc1/docker-compose.yml up -d
