30b6269bde040cca36a09fdbfa5bae7e810cb879
Mirko Kämpf <mirko@confluent.io>
2020-06-10 15:40:14 +0200

    workaround for JAR in gitignore

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
@@ -15,9 +15,9 @@ rm -rf tmp_consumer1
 rm -rf tmp_consumer2
 rm -rf tmp_consumer3
 
-now=$(date);sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1-$now --show-detailed-stats --broker-list broker-west-5:9091 --reporting-interval 100 --hide-header > tmp_consumer1
-now=$(date);sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-2-$now --show-detailed-stats --broker-list broker-west-6:9092 --reporting-interval 100 --hide-header > tmp_consumer2
-now=$(date);sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-3-$now --show-detailed-stats --broker-list broker-west-7:9093 --reporting-interval 100 --hide-header > tmp_consumer3
+now=$(date +%s);sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1-$now --show-detailed-stats --broker-list broker-west-5:9091 --reporting-interval 100 --hide-header > tmp_consumer1
+now=$(date +%s);sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-2-$now --show-detailed-stats --broker-list broker-west-6:9092 --reporting-interval 100 --hide-header > tmp_consumer2
+now=$(date +%s);sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-3-$now --show-detailed-stats --broker-list broker-west-7:9093 --reporting-interval 100 --hide-header > tmp_consumer3
 
 cat tmp_consumer1
 echo
