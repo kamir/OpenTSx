349ee7c81739f29b1179e9a7af462ea47f2bbfdc
Mirko Kämpf <mirko@confluent.io>
2020-06-10 15:52:37 +0200

    workaround for JAR in gitignore

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/clean.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/clean.sh
@@ -1,6 +1,7 @@
 cd ../..
 
 sudo docker-compose exec cli-west-1 kafka-topics --delete --topic test-szenario-01 --bootstrap-server 192.168.0.9:9091
+sudo docker-compose exec cli-west-1 kafka-topics --delete --topic test-szenario-02 --bootstrap-server 192.168.0.9:9091
 
 sudo docker-compose exec cli-west-1 kafka-topics --list --bootstrap-server 192.168.0.9:9091
 
