5649b37798e1642cd35483db570f85993630ac87
Mirko Kämpf <mirko@confluent.io>
2020-06-10 15:10:32 +0200

    workaround for JAR in gitignore

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
@@ -15,9 +15,9 @@ rm -rf tmp_consumer1
 rm -rf tmp_consumer2
 rm -rf tmp_consumer3
 
-sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-1:9091 > tmp_consumer1
-sudo docker-compose exec cli-west-2 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-2 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-2:9092 > tmp_consumer2
-sudo docker-compose exec cli-west-3 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-3 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-1:9091 > tmp_consumer3
+sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-1:9091 > tmp_consumer1 &
+sudo docker-compose exec cli-west-2 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-2 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-2:9092 > tmp_consumer2 &
+sudo docker-compose exec cli-west-3 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-3 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-1:9091 > tmp_consumer3 &
 
 cat tmp_consumer1
 cat tmp_consumer2
--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/up.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/up.sh
@@ -4,4 +4,4 @@ sudo docker-compose exec cli-west-1 kafka-topics --create --topic test-szenario-
 
 sudo docker-compose exec cli-west-1 kafka-topics --list --bootstrap-server 192.168.0.9:9091
 
-sudo docker-compose exec cli-west-1 kafka-producer-perf-test --topic test-szenario-01 --num-records 50000000 --record-size 100 --throughput -1 --producer-props acks=1 bootstrap.servers=192.168.0.9:9091 buffer.memory=67108864 batcsize=8196
+sudo docker-compose exec cli-west-1 kafka-producer-perf-test --topic test-szenario-01 --num-records 5000 --record-size 100 --throughput -1 --producer-props acks=1 bootstrap.servers=192.168.0.9:9091 buffer.memory=67108864 batch.size=8196
