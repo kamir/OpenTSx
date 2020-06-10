e33af84626f8b80b5a3eaf1d048f1b4bca1dc49d
Mirko Kämpf <mirko@confluent.io>
2020-06-10 15:27:54 +0200

    workaround for JAR in gitignore

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/up.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/up.sh
@@ -1,9 +1,14 @@
 cd ../..
 
+###
+#
+# https://gist.github.com/ueokande/b96eadd798fff852551b80962862bfb3
+#
+##
 sudo docker-compose exec cli-west-1 kafka-topics --create --topic test-szenario-01 --bootstrap-server 192.168.0.9:9091
 
 sudo docker-compose exec cli-west-1 kafka-topics --list --bootstrap-server 192.168.0.9:9091
 
-sudo docker-compose exec cli-west-1 kafka-producer-perf-test --topic test-szenario-01 --num-records 5000 --record-size 100 --throughput -1 --producer-props acks=1 bootstrap.servers=192.168.0.9:9091 buffer.memory=67108864 batch.size=8196
+sudo docker-compose exec cli-west-1 kafka-producer-perf-test --topic test-szenario-01 --num-records 50000 --record-size 100 --throughput -1 --producer-props acks=1 bootstrap.servers=192.168.0.9:9091 buffer.memory=67108864 batch.size=8196
 
-sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic test-szenario-01 --messages 5000 --group=perf-test-3 --show-detailed-stats --broker-list broker-west-1:9091
+now=$(date);sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --print-metrics --topic test-szenario-01 --messages 50000 --group=perf-test-3-$now --show-detailed-stats --broker-list broker-west-1:9091 --reporting-interval 500
