eac48eb337c82aa4d393c1acc422efb4b0bf9f5a
Mirko Kämpf <mirko@confluent.io>
2020-06-10 15:14:31 +0200

    workaround for JAR in gitignore

--- opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
+++ opentsx-clusters/mmdc-mrc/mdc1/scripts/scenario1/run.sh
@@ -1,5 +1,6 @@
 export TN=test-szenario-01
 export ZM=5000
+export D=$(time)
 
 cd ../..
 
@@ -15,7 +16,7 @@ rm -rf tmp_consumer1
 rm -rf tmp_consumer2
 rm -rf tmp_consumer3
 
-sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-1:9091
+sudo docker-compose exec cli-west-1 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-1-$D --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-1:9091
 #sudo docker-compose exec cli-west-2 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-2 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-2:9092 > tmp_consumer2
 #sudo docker-compose exec cli-west-3 kafka-consumer-perf-test --topic $TN --messages $ZM --group=perf-test-3 --show-detailed-stats --hide-header --timeout 60000 --broker-list broker-west-1:9091 > tmp_consumer3
 
