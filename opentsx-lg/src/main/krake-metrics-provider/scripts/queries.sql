40001959d7792d4334a48ac8b095f6a4e732a49d
MK <mirko@confluent.io>
2020-07-09 15:33:15 +0000

    created the solution for Krake metrics provider

--- opentsx-lg/src/main/krake-metrics-provider/scripts/queries.sql
+++ opentsx-lg/src/main/krake-metrics-provider/scripts/queries.sql
@@ -12,4 +12,4 @@ WITH (
      KEY='metric'
 );
 CREATE TABLE kmt WITH (KAFKA_TOPIC='topic_kmt', VALUE_FORMAT='AVRO') as select * from krake_metrics;
-create table kvt with (KAFKA_TOPIC='topic_kvt', VALUE_FORMAT='AVRO') as select metric, COLLECT_LIST(value)[0] AS value from km group by metric;
+create table kvt with (KAFKA_TOPIC='topic_kvt', VALUE_FORMAT='AVRO') as select metric, COLLECT_LIST(value)[0] AS value from kmt group by metric;
