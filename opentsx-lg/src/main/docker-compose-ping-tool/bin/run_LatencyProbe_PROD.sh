40a095490022cadd97641242c84a16338f4e2856
Mirko Kämpf <mirko@confluent.io>
2020-10-14 17:59:00 +0200

    Updated CP 5.5.0 to 6.0.0.

--- opentsx-lg/src/main/docker-compose-ping-tool/bin/run_LatencyProbe_PROD.sh
+++ opentsx-lg/src/main/docker-compose-ping-tool/bin/run_LatencyProbe_PROD.sh
@@ -5,5 +5,4 @@ echo "------------------------------------------"
 echo " Start the LATENCY analysis ... PRODUCER "
 echo "------------------------------------------"
 
-#java -javaagent:./bin/jmx_prometheus_javaagent-0.13.0.jar=8080:./bin/prometheus_config.yaml -cp target/opentsx-lg-3.0.1.jar org.opentsx.lg.EventFlowAnalysisProducer
-mvn clean compile package exec:java -Dexec.mainClass="org.opentsx.lg.kping.EventFlowAnalysisProducer"
+java -cp /opentsx-lg/opentsx-lg-3.0.1.jar org.opentsx.lg.kping.EventFlowAnalysisProducer
